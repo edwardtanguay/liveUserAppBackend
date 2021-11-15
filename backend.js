6c2f52ad6d757dcd1d206dc44b3d84a96676bdc1
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-15 14:16:27 +0100

    delete works

--- backend.js
+++ backend.js
@@ -1,19 +1,19 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import mongodb, { MongoClient } from 'mongodb';
 
 const app = express();
 const port = 3022;
 const mongoConnectionString = 'mongodb://localhost:27017';
 const client = new MongoClient(mongoConnectionString);
 
-const getData = async (done) => {
+const execMongo = async (done) => {
 	await client.connect();
 	const db = client.db('api002');
 	done(db);
 }
 
 app.get('/', (req, res) => {
-	getData(async (db) => {
+	execMongo(async (db) => {
 		const users = await db.collection('users100').find()
 			.project({
 				name: 1,
@@ -28,7 +28,12 @@ app.get('/', (req, res) => {
 
 app.delete('/deleteuser/:id', (req, res) => {
 	const id = req.params.id;
-	res.send(id);
+	execMongo(async (db) => {
+		const deleteResult = await db.collection('users100').deleteOne({ _id: new mongodb.ObjectId(id) });
+		res.json({
+			result: deleteResult
+		})
+	});
 });
 
 app.listen(port, () => {
