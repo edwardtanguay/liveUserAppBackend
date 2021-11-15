2187a637cf35aeb11fd2ddff27860cbcbcdb041c
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-15 13:52:14 +0100

    delete test works

--- backend.js
+++ backend.js
@@ -1,10 +1,34 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 const port = 3022;
+const mongoConnectionString = 'mongodb://localhost:27017';
+const client = new MongoClient(mongoConnectionString);
+
+const getData = async (done) => {
+	await client.connect();
+	const db = client.db('api002');
+	done(db);
+}
 
 app.get('/', (req, res) => {
-	res.send('it works');
+	getData(async (db) => {
+		const users = await db.collection('users100').find()
+			.project({
+				name: 1,
+				username: 1,
+				email: 1
+			})	
+			.toArray();
+		res.json(users);
+	});
+});
+
+
+app.delete('/deleteuser/:id', (req, res) => {
+	const id = req.params.id;
+	res.send(id);
 });
 
 app.listen(port, () => {
